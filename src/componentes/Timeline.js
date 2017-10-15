36a269aa842a17b2170c35be17f0b239773ab3e9
Lianker <near.7@hotmail.com>
2017-10-15 21:24:26 -0200

    basic

--- src/componentes/Timeline.js
+++ src/componentes/Timeline.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import FotoItem from "./FotoItem";
-import Pubsub from "pubsub-js";
-// import ReactCSSTransitionGroup from "react/lib/ReactCSSTransitionGroup";
+// import ReactCSSTransitionGroup from 'react/lib/ReactCSSTransitionGroup';
 
 export default class Timeline extends Component {
   constructor(props) {
@@ -11,29 +10,8 @@ export default class Timeline extends Component {
   }
 
   componentWillMount() {
-    Pubsub.subscribe("timeline", (topico, fotos) => {
+    this.props.store.subscribe(fotos => {
       this.setState({ fotos });
-    });
-
-    Pubsub.subscribe("atualiza-liker", (topico, infoLiker) => {
-      const fotoAchada = this.state.fotos.find(foto => foto.id === infoLiker.fotoId);
-      fotoAchada.likeada = !fotoAchada.likeada;
-
-      const possivelLiker = fotoAchada.likers.find(liker => liker.login === infoLiker.liker.login);
-
-      if (possivelLiker === undefined) {
-        fotoAchada.likers.push(infoLiker.liker);
-      } else {
-        const novosLikers = fotoAchada.likers.filter(liker => liker.login !== infoLiker.liker.login);
-        fotoAchada.likers = novosLikers;
-      }
-      this.setState({ fotos: this.state.fotos });
-    });
-
-    Pubsub.subscribe("novos-comentarios", (topico, infoComentario) => {
-      const fotoAchada = this.state.fotos.find(foto => foto.id === infoComentario.fotoId);
-      fotoAchada.comentarios.push(infoComentario.novoComentario);
-      this.setState({ fotos: this.state.fotos });
     });
   }
 
@@ -45,12 +23,7 @@ export default class Timeline extends Component {
     } else {
       urlPerfil = `http://localhost:8080/api/public/fotos/${this.login}`;
     }
-
-    fetch(urlPerfil)
-      .then(response => response.json())
-      .then(fotos => {
-        this.setState({ fotos: fotos });
-      });
+    this.props.store.lista(urlPerfil);
   }
 
   componentDidMount() {
@@ -65,52 +38,24 @@ export default class Timeline extends Component {
   }
 
   like(fotoId) {
-    fetch(`http://localhost:8080/api/fotos/${fotoId}/like?X-AUTH-TOKEN=${localStorage.getItem("auth-token")}`, {
-      method: "POST",
-    })
-      .then(response => {
-        if (response.ok) {
-          return response.json();
-        } else {
-          throw new Error("não foi possível realizar o like da foto");
-        }
-      })
-      .then(liker => {
-        Pubsub.publish("atualiza-liker", { fotoId, liker });
-      });
+    this.props.store.like(fotoId);
   }
 
   comenta(fotoId, textoComentario) {
-    const requestInfo = {
-      method: "POST",
-      body: JSON.stringify({ texto: textoComentario }),
-      headers: new Headers({
-        "Content-type": "application/json",
-      }),
-    };
-
-    fetch(
-      `http://localhost:8080/api/fotos/${fotoId}/comment?X-AUTH-TOKEN=${localStorage.getItem("auth-token")}`,
-      requestInfo
-    )
-      .then(response => {
-        if (response.ok) {
-          return response.json();
-        } else {
-          throw new Error("não foi possível comentar");
-        }
-      })
-      .then(novoComentario => {
-        Pubsub.publish("novos-comentarios", { fotoId, novoComentario });
-      });
+    this.props.store.comenta(fotoId, textoComentario);
   }
 
   render() {
     return (
       <div className="fotos container">
-        {this.state.fotos.map(foto => <FotoItem key={foto.id} foto={foto} like={this.like} comenta={this.comenta} />)}
-        {/* <ReactCSSTransitionGroup transitionName="timeline" transitionEnterTimeout={500} transitionLeaveTimeout={300}>
-        </ReactCSSTransitionGroup> */}
+        {/* <ReactCSSTransitionGroup
+          transitionName="timeline"
+          transitionEnterTimeout={500}
+          transitionLeaveTimeout={300}> */}
+        {this.state.fotos.map(foto => (
+          <FotoItem key={foto.id} foto={foto} like={this.like.bind(this)} comenta={this.comenta.bind(this)} />
+        ))}
+        {/* </ReactCSSTransitionGroup>         */}
       </div>
     );
   }
